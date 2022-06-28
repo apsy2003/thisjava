cce68c8ac44cb0ffb8181859cb10773db8438d45
apsy2003 <apsy2003naver.com>
2022-06-28 13:01:10 +0900

    DenyLogicOperatorExample

--- chap03/src/sec03/exam02_increase_decrease/IncreaseDecreaseOperatorExample.java
+++ chap03/src/sec03/exam02_increase_decrease/IncreaseDecreaseOperatorExample.java
@@ -10,28 +10,28 @@ public class IncreaseDecreaseOperatorExample {
 		System.out.println("-------------------------");
 		x++;
 		++x;
-		System.out.println("x=" + x);
+		System.out.println("x=" + x); // x = 12
 		
 		System.out.println("-------------------------");
 		y--;
 		--y;
-		System.out.println("y=" + y);
+		System.out.println("y=" + y); // y = 8
 		
 		System.out.println("-------------------------");
-		z = x++;
-		System.out.println("z=" + z);
-		System.out.println("x=" + x);
+		z = x++; 					  // z = 12(13)
+		System.out.println("z=" + z); 
+		System.out.println("x=" + x); // x = 13
 
 		System.out.println("-------------------------");
-		z = ++x;
+		z = ++x;					 // z = 14
 		System.out.println("z=" + z);
-		System.out.println("x=" + x);
+		System.out.println("x=" + x); // x = 14
 		
 		System.out.println("-------------------------");
-		z = ++x + y++;
-		System.out.println("z=" + z);
-		System.out.println("x=" + x);
-		System.out.println("y=" + y);
+		z = ++x + y++; // 23 = 15 + 8(9)
+		System.out.println("z=" + z); // 23
+		System.out.println("x=" + x); // 15
+		System.out.println("y=" + y); // 9
 		
 	}
 
