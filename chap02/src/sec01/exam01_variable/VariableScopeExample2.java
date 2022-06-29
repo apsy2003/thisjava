6a7257ea6e8888985ee37b0a4824b9a2b9cb715d
apsy2003 <apsy2003naver.com>
2022-06-29 09:36:46 +0900

    ArtihemticOperatorExample

--- chap02/src/sec01/exam01_variable/VariableScopeExample2.java
+++ chap02/src/sec01/exam01_variable/VariableScopeExample2.java
@@ -5,7 +5,7 @@ public class VariableScopeExample2 {
 	public static void main(String[] args) {
 		int v1 = 15;
 		if(v1>10){
-			int v2;
+		int v2;
 			v2 = v1 - 10;
 		}
 
--- chap03/src/sec03/exam04_bit_reverse/BitReverseOperatorExample.java
+++ chap03/src/sec03/exam04_bit_reverse/BitReverseOperatorExample.java
@@ -4,7 +4,7 @@ public class BitReverseOperatorExample {
 
 	public static void main(String[] args) {
 		int v1 = 10;
-		int v2 =~v1;
+		int v2 = ~v1;
 		int v3 = ~v1 + 1;
 		System.out.println(toBinaryString(v1) + " (십진수: " + v1 +")");
 		System.out.println(toBinaryString(v2) + " (십진수: " + v2 +")");
