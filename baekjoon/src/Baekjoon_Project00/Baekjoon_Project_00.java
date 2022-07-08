eb3dc7046ce5012f3bf6683396c12aff0236a670
apsy2003 <apsy2003naver.com>
2022-07-08 09:47:25 +0900

    ComputerExample

---
+++ baekjoon/src/Baekjoon_Project00/Baekjoon_Project_00.java
@@ -0,0 +1,27 @@
+package Baekjoon_Project00;
+
+import java.util.Scanner;
+
+public class Baekjoon_Project_00 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int h = 0;
+		int m = 0;
+		
+		h = sc.nextInt();
+		m = sc.nextInt();
+		
+		if(m < 45) {
+			h = h - 1;
+			m = 60 - (45 - m);
+			if(h < 0) {
+				h = 23;
+			}
+			System.out.println(h + " " + m);
+		} else {
+			System.out.println(h + " " + (m-45));		
+			}
+	}
+}
