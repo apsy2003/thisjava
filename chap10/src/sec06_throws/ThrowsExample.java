73d4d080c81d52ba8af1c1c56ed1411c67065d01
apsy2003 <apsy2003naver.com>
2022-07-28 10:45:27 +0900

    sec07_AccountExample(예외오류발생)

--- chap10/src/sec06_throws/ThrowsExample.java
+++ chap10/src/sec06_throws/ThrowsExample.java
@@ -3,8 +3,15 @@ package sec06_throws;
 public class ThrowsExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			findClass();
+		} catch(ClassNotFoundException e) {
+			System.out.println("클래스가 존재하지 않습니다.");
+		}
 	}
 
+	public static void findClass() throws ClassNotFoundException{//클래스에서는 이런 에러가 발생할 수 있습니다.를 의미★
+		Class clazz = Class.forName("java.lang.String2");
+				// ClassNotFoundException을 써주어야 실행을 진행할 수 있음. 예외 문을 모아다가 사용.
+	}
 }
