ba44f7fb2a8b33e5b7e3bf6bac18c7bc7d039193
apsy2003 <apsy2003naver.com>
2022-07-29 09:59:07 +0900

    chap11 Member

---
+++ chap11/src/sec01/exam01_equals/Member.java
@@ -0,0 +1,19 @@
+package sec01.exam01_equals;
+
+public class Member {
+	public String id; //id는 필드가 지정해준 값.
+	
+	public Member(String id) {//필드 변수에 따라 넘겨준다.
+		this.id = id;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member) obj;
+			if(id.equals(member.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
