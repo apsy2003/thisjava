acbc720108dd95adbd719ac6a3889627afdea938
apsy2003 <apsy2003naver.com>
2022-08-02 11:02:05 +0900

    StringToLowerUpperCaseExample

---
+++ chap11/src/sec07/exam02_method/StringToLowerUpperCaseExample.java
@@ -0,0 +1,25 @@
+package sec07.exam02_method;
+
+public class StringToLowerUpperCaseExample {
+
+	public static void main(String[] args) {
+		String str1 = "Java Programing";
+		String str2 = "JAVA Programing";
+		
+		System.out.println(str1.equals(str2));
+		
+		String lowerStr1 = str1.toLowerCase();
+		String lowerStr3 = str1.toUpperCase();
+		System.out.println(lowerStr1);
+		System.out.println(lowerStr3);
+		String lowerStr2 = str2.toLowerCase();
+		String lowerStr4 = str2.toUpperCase();
+		System.out.println(lowerStr2);
+		System.out.println(lowerStr4);
+		System.out.println(lowerStr1.equals(lowerStr2));
+		
+		System.out.println(str1.equalsIgnoreCase(str2));
+
+	}
+
+}
