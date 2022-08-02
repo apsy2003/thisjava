50a9a924f0f3b6b24278281b4e4d12a0ada979ba
apsy2003 <apsy2003naver.com>
2022-08-02 09:56:26 +0900

    method

--- chap11/src/sec07/exam01_contstructor/exam01_constructor.java
+++ chap11/src/sec07/exam01_contstructor/exam01_constructor.java
@@ -7,7 +7,7 @@ public class exam01_constructor {
 	public static void main(String[] args) throws IOException{
 		byte[] bytes = new byte[100];
 		
-		System.out.println("입력: ");
+		System.out.print("입력: ");
 		int readByteNo = System.in.read(bytes);
 		
 		String str = new String(bytes, 0, readByteNo-2);
