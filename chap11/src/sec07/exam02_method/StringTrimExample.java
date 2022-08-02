0194ed7b3a798f27e40616501aa02bd7d188a836
apsy2003 <apsy2003naver.com>
2022-08-02 11:09:36 +0900

    StringValueOfExample

--- chap11/src/sec07/exam02_method/StringTrimExample.java
+++ chap11/src/sec07/exam02_method/StringTrimExample.java
@@ -6,7 +6,7 @@ public class StringTrimExample {
 		String tel1 = " 02";
 		String tel2 = "123 ";
 		String tel3 = " 1234 ";
-		
+		//공백을 없애줌 Trim
 		String tel = tel1.trim() + tel2.trim() + tel3.trim();
 		System.out.println(tel);
 	}
